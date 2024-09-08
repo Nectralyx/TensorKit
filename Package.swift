74d9a17f71fe76f6910fbf7e5d86e8b9d968ea8c
Powerchip15 <130801086+Powerchip15@users.noreply.github.com>
2024-09-07 18:15:50 -0600

    Optimization

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TensorKit",
-            type: .dynamic,
+            type: .static,
             targets: ["TensorKit"]),
     ],
     targets: [
